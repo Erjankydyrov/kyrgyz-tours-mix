3af17d5336908d1aae4a7f0fdfd84ffb4beee9a2
Erjankydyrov <erjankydyrov29@gmail.com>
2021-09-27 15:24:42 +0600

    added 4 pages and elements

--- src/app.js
+++ src/app.js
@@ -1,1 +1,48 @@
 import 'bootstrap';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import '../node_modules/swiper/swiper-bundle';
+
+const swiper = new Swiper('.swiper', {
+  // centeredSlides: true,
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 480px
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    },
+    // when window width is >= 640px
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 60
+    },
+    // when window width is >= 640px
+    1850: {
+      slidesPerView: 5,
+      spaceBetween: 60
+    },
+    // when window width is >= 640px
+    2350: {
+      slidesPerView: 6,
+      spaceBetween: 60
+    },
+  }
+})
